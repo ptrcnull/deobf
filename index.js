7c39671acdef716e281f0c75952a810cfaff5f3e
ptrcnull <git@ptrcnull.me>
2021-12-13 14:13:24 +0100

    feat: Add arrayBool

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const fs = require('fs-extra')
 const arrayObfuscation = require('./lib/arrayObfuscation')
 const squareBrackets = require('./lib/squareBrackets')
 const requireNames = require('./lib/requireNames')
+const arrayBool = require('./lib/arrayBool')
 
 async function main () {
   const file = await fs.readFile(process.argv[2], 'utf8')
@@ -12,6 +13,7 @@ async function main () {
   parsed = arrayObfuscation(parsed)
   parsed = squareBrackets(parsed)
   parsed = requireNames(parsed)
+  parsed = arrayBool(parsed)
 
   if (process.argv[3]) console.log(eval(process.argv[3])) // eslint-disable-line no-eval
 
